c60cdf917dc53b0e65f7b0eba2bd4b9eb4f1cc6a
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2021-12-26 22:54:54 +0530

    updated auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import 'express-async-errors';
+import {currentUserRouter} from "./routes/current-user";
+import {signinRouter} from "./routes/signin";
+import {signoutRouter} from "./routes/signout";
+import {signupRouter} from "./routes/signup";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/api/users/currentuser", (req, res) => {res.send("Hi")});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {throw new NotFoundError();})
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on 3000 !!");
