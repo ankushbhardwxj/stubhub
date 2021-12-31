4c7b1fe20b105f4c57e4c9b65caf704fbe822d04
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2021-12-31 22:26:12 +0530

    tickets service done

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,9 @@ import cookieSession from "cookie-session";
 import 'express-async-errors';
 import {errorHandler, NotFoundError} from "@ankushstubhub/common";
 import {createTicketRouter} from "./routes/new";
+import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes";
+import {updateTicketRouter} from "./routes/update";
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,8 +15,11 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== "test"
 }));
-app.use(createTicketRouter);
 
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {throw new NotFoundError();})
 app.use(errorHandler);
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,16 +1,21 @@
 import express, {Request, Response} from "express";
 import {requireAuth, validateRequest} from "@ankushstubhub/common";
 import {body} from "express-validator";
+import {Ticket} from "../models/ticket";
 
 const router = express.Router();
 
 const validator = [
-  body('title').not().isEmpty().withMessage("Title is required")
+  body('title').not().isEmpty().withMessage("Title is required"),
+  body('price').isFloat({gt: 0}).withMessage("Price must be greater than 0")
 ]
 
 router.post("/api/tickets", requireAuth, validator, validateRequest,
   async (req: Request, res: Response) => {
-    res.sendStatus(200);
+    const {title, price} = req.body;
+    const ticket = Ticket.build({title, price, userId: req.currentUser!.id});
+    await ticket.save();
+    res.status(201).send(ticket);
   })
 
 export {router as createTicketRouter};
